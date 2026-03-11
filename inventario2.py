08ee70d7a390dd78157dc86f3e2c5a1e89d3bbe7
MauriDavi20 <mauricioposada08@gmail.com>
2026-03-11 10:47:53 -0500

    Add files via upload

--- inventario2.py
+++ inventario2.py
@@ -1,4 +1,4 @@
-menu = []
+inventario = []
 
 print("1. Agregar producto")
 print("2. Mostrar inventario")
@@ -9,14 +9,27 @@ while True:
  opcion = (input("Seleccione una opción: "))
 
  if opcion == "1":
-    producto = input("Ingrese el nombre del producto: ")
-    menu.append(producto)
+  def agregar():
+   nombre = (input("Ingrese el nombre del producto: "))
+   precio = int(input("ingrese el precio del producto: "))
+   cantidad = int(input("ingrese la cantidad deseada: "))
+   producto = {"nombre":nombre,"precio":precio,"cantidad":cantidad,}
+   inventario.append(producto)
 
  elif opcion == "2":
-    print(menu)
-
- elif opcion == "3":
-    print("Cantidad de productos:", len(menu))
+    def mostrar():
+     print(inventario)
+     for i, producto in enumerate(inventario, start=1):
+      print(f"{i}. Nombre: {producto['nombre']}, Precio: {producto['precio']}, Cantidad: {producto['cantidad']}")
+      if inventario == []:
+       print("el inventario esta vacio")
+        
+ elif opcion == "3": 
+     def calcular():
+      valor_total = sum(i["cantidad"] * i["precio"] for i in inventario)
+      print("valor del total de productos en el inventario:", valor_total)
+      total_productos = len(inventario)
+      print("cantidad total de productos registrados en el inventario:",total_productos)
 
  elif opcion == "4":
     print("Saliendo del programa")
